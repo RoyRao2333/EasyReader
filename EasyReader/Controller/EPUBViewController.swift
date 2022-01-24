6116c2c1e1a424cd3855ddca226537528828206b
RoyRao2333 <royrao2333@gmail.com>
2022-01-24 14:05:04 +0800

    Add page curl effect to webView

--- EasyReader/Controller/EPUBViewController.swift
+++ EasyReader/Controller/EPUBViewController.swift
@@ -8,22 +8,35 @@
 import UIKit
 import WebKit
 import EPUBKit
+import SnapKit
 
 class EPUBViewController: UIViewController {
-    @IBOutlet weak var webView: WKWebView!
     @IBOutlet private var backBtn: UIButton!
     
+    private var pageViewController: UIPageViewController!
+    
     var epubDocument: EPUBDocument?
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    private var toc: [EPUBTableOfContents] = []
+    private var pageVCs: [UIViewController] = []
+    private var currentPage = 0 {
+        didSet {
+            let range = (currentPage - 1) ... (currentPage + 1)
+            
+            for (index, vc) in pageVCs.enumerated() {
+                if range ~= index {
+                    vc.loadViewIfNeeded()
+                } else {
+                    vc.view = nil
+                }
+            }
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        return2Toc(backBtn)
+        toc = processToc()
+        setup()
     }
 }
 
@@ -33,10 +46,10 @@ extension EPUBViewController {
     
     @IBAction private func return2Toc(_ sender: UIButton) {
         if
-            let fileURL = epubDocument?.directory.appendingPathComponent("nav.xhtml", isDirectory: false),
+            let fileURL = epubDocument?.contentDirectory.appendingPathComponent("nav.xhtml", isDirectory: false),
             let documentURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
         {
-            webView.loadFileURL(fileURL, allowingReadAccessTo: documentURL)
+            
         }
     }
     
@@ -44,5 +57,116 @@ extension EPUBViewController {
         dismiss(animated: true) { [weak self] in
             self?.epubDocument = nil
         }
+    }
+}
+
+
+// MARK: Private Methods -
+extension EPUBViewController {
+    
+    private func setup() {
+        pageViewController = UIPageViewController(
+            transitionStyle: .pageCurl,
+            navigationOrientation: .horizontal,
+            options: nil
+        )
+        pageViewController.delegate = self
+        pageViewController.dataSource = self
+        pageViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        addChild(pageViewController)
+        view.addSubview(pageViewController.view)
+        pageViewController.view.snp.makeConstraints { make in
+            make.leading.trailing.bottom.equalToSuperview()
+            make.top.equalTo(backBtn.snp.bottom).offset(15)
+        }
+        
+        toc.forEach {
+            if
+                let itemPath = $0.item,
+                let fileURL = epubDocument?.contentDirectory.appendingPathComponent(itemPath)
+            {
+                pageVCs.append(createWebVC(for: fileURL))
+            }
+        }
+        
+        pageViewController.setViewControllers(
+            [pageVCs[0]],
+            direction: .forward,
+            animated: false
+        )
+    }
+    
+    private func createWebVC(for fileURL: URL) -> UIViewController {
+        let documentURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+        
+        let preferences = WKWebpagePreferences()
+        preferences.allowsContentJavaScript = true
+        let configuration = WKWebViewConfiguration()
+        configuration.defaultWebpagePreferences = preferences
+        
+        let webView = WKWebView(frame: .zero, configuration: configuration)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.loadFileURL(fileURL, allowingReadAccessTo: documentURL ?? fileURL)
+        
+        let vc = UIViewController()
+        vc.view.addSubview(webView)
+        webView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        return vc
+    }
+    
+    private func processToc(_ tocArray: [EPUBTableOfContents]? = nil) -> [EPUBTableOfContents] {
+        guard let tocArray = tocArray ?? epubDocument?.tableOfContents.subTable else { return [] }
+        
+        var result: [EPUBTableOfContents] = []
+        tocArray.forEach {
+            if let nested = $0.subTable, !nested.isEmpty {
+                result.append(contentsOf: processToc(nested))
+            } else {
+                result.append($0)
+            }
+        }
+        
+        return result
+    }
+}
+
+
+// MARK: PageViewController Delegate
+extension EPUBViewController: UIPageViewControllerDelegate {
+    
+}
+
+
+// MARK: PageViewController DataSource
+extension EPUBViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let index = pageVCs.firstIndex(of: viewController), index > 0 else { return nil }
+//
+//        currentPage = index - 1
+//
+        return pageVCs[index - 1]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let index = pageVCs.firstIndex(of: viewController), index < pageVCs.count - 1 else { return nil }
+        
+//        currentPage = index + 1
+        
+        return pageVCs[index + 1]
+    }
+}
+
+
+extension EPUBViewController {
+    
+    private enum PageNav: String, Hashable {
+        case prev = "prev"
+        case current = "current"
+        case next = "next"
     }
 }
